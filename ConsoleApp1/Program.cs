2ae2129dec9c7ecf13eef87cfc893d37798389d0
mrerikk <37010845+mrerikk@users.noreply.github.com>
2022-06-16 15:16:03 +0300

    Created program
    Created program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Line to count words");
+
+            Console.WriteLine (CountWords(Console.ReadLine()));
+
+            Console.ReadKey();
+
+        }
+
+        public static int CountWords(string input)
+                {
+                    var found = false;
+                    var count = 0;
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (input[i] != ' ')
+                        {
+                            if (found == false)
+                            {
+                                count++;
+                            }
+                            found = true;
+                        }
+                        else
+                        {
+                            found = false;
+                        }
+                    }
+
+
+                    return count;
+                }
+        
+    }
+}
